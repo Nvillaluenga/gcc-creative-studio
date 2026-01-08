c0495b09dcfea46fa31b5e2d8b6e3730fc3133c6
UMA MAHESWAR REDDY TARUGU <umamaheswar@google.com>
2026-01-08 10:20:43 +0000

    add backend support for audio and vedio upload to gcp from workbench

--- backend/src/common/base_dto.py
+++ backend/src/common/base_dto.py
@@ -25,6 +25,10 @@ class MimeTypeEnum(str, Enum):
     IMAGE_PNG = "image/png"
     VIDEO_MP4 = "video/mp4"
     AUDIO_WAV = "audio/wav"
+    AUDIO_MPEG = "audio/mpeg"
+    AUDIO_MP3 = "audio/mp3"
+    AUDIO_OGG = "audio/ogg"
+    AUDIO_WEBM = "audio/webm"
 
 
 class GenerationModelEnum(str, Enum):
